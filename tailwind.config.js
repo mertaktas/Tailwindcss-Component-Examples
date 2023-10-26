0e8c826419aa10125c054ffa3f3b213722de4ba5
mertaktas <mertaktas231@gmail.com>
2023-10-27 00:38:59 +0300

    Example-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0px 12px 92px 0px rgba(25, 27, 74, 0.10)',
+        'custom2': '0px 7px 20px 0px rgba(115, 116, 156, 0.08)'
+
+      }
+    },
   },
   plugins: [],
 }
